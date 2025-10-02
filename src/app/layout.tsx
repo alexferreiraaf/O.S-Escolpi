2ede4880a0973d256f64a2c2406c40a5c5e271e9
Alex Ferreira <alexescolpi@gmail.com>
2025-10-02 15:52:36 +0000

    Eu quero essa aplicação em pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from '@/components/ui/toaster';
 export const metadata: Metadata = {
   title: 'Service Order Central',
   description: 'Gestão de Ordens de Serviço - Escolpi Informática',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -23,6 +24,17 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
           rel="stylesheet"
         />
+        <meta name="application-name" content="Service Order Central" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Service Order Central" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
       </head>
       <body className="font-body antialiased">
         <ThemeProvider attribute="class" defaultTheme="light">
