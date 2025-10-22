6e6ba81497c689e0e549c6da78b50433b05062d1
Alex Ferreira <alexescolpi@gmail.com>
2025-10-22 17:59:00 +0000

    Quando eu instaltar o app eu quero o novo nome também.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,30 @@
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  manifest: {
+    name: 'O.S Escolpi',
+    short_name: 'O.S Escolpi',
+    description: 'Gestão de Ordens de Serviço - Escolpi Informática',
+    theme_color: '#000000',
+    background_color: '#ffffff',
+    icons: [
+        {
+            "src": "/icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "/icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ]
+  }
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
@@ -31,4 +58,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
