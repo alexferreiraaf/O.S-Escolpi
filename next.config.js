4836ffaec62da8814f67483bf038a70729475471
Alex Ferreira <alexescolpi@gmail.com>
2025-10-22 11:21:19 +0000

    Porque aqui no studio está funcionando e pela vercel não?

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
@@ -39,4 +31,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
